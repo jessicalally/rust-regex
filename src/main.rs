fd545219d87042eda11d79045e9cac4d3859d880
Jessica Lally <jml19@ic.ac.uk>
2020-10-19 17:35:29 +0100

    Implement main function to accept command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use std::env;
+use rust_regex::lexer::lex;
+use rust_regex::parser::parse;
+use rust_regex::matcher::find;
+
+fn main() -> Result<(), &'static str> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("Error: too few arguments\nUsage: ./rust-regex regex_pattern input_string");
+    }
+
+    let pattern = &args[1];
+    let input = &args[2];
+
+    println!("{:?}", find(&parse(&lex(pattern)?)?, input));
+    Ok(())
+}
